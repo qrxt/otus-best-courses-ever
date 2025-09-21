68466fdccaa9efff6feacd2b1bcb31d922fc098b
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2025-09-21 06:30:19 +0300

    Добавление начального приложения

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,18 @@
-console.log('hii');
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { type Express } from 'express';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.set('trust proxy', true);
+
+// middlewares
+app.use(express.json());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
+app.listen(process.env.PORT, () => {
+  const { NODE_ENV, HOST, PORT } = process.env;
+  console.log(`Сервер (${NODE_ENV}) запущен: http://${HOST}:${PORT}`);
+});
